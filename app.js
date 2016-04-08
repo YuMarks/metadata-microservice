754bbd5a15d9a73ec0056a974a5aeba98f69b009
ymarks <amy.neal@rocketmail.com>
2016-04-08 18:18:53 +0000

    finished project

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
 var express = require('express'),
-    app = express(),
-    path = require('path'),
-    multer = require('multer'),
-    upload = multer({dest: 'uploads/'}),
-    path = require("path"),
-    port = port = process.env.PORT || 8080;
-    
+  app = express(),
+  http = require('http'),
+  fs = require('fs'),
+  path = require('path'),
+  multer = require('multer'),
+  upload = multer({
+    dest: 'uploads/'
+  }),
+  fileName = '',
+  filePath = '',
+  fileSize = '',
+  port = port = process.env.PORT || 8080;
 
-    app.get('/',function(req,res){ 
-        res.sendFile(path.join(__dirname+'/index.html'));
-        }); 
-        app.post('/formHandler', upload.single('myFile'), 
-        function(req, res) { 
-            console.log(req.file); res.json( req.file.size ); }); 
-            app.listen(port);
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+app.post('/formHandler', upload.single('myFile'),
+  function(req, res) {
+    if (req.file == undefined) {
+      res.write("<script type = 'application/javascript'>alert('No file uploaded')</script>" + "<p><form method= 'POST' enctype='multipart/form-data' action='/formHandler'><input type='file' name='myFile'><input type='submit' name='submit' value= 'Upload'></form> </p>");
+      res.end();
+    }
+    fileName = req.file.filename;
+    filePath = path.join(__dirname + '/uploads/' + fileName);
+    fs.unlinkSync(filePath);
+    fileSize = req.file.size;
+    var phrase = "File Size: " + fileSize;
+    var alertText = "<script type = 'application/javascript'>alert(" + '"' + phrase + '"' + " );</script>";
+    res.write(alertText + "<p><form method= 'POST' enctype='multipart/form-data' action='/formHandler'><input type='file' name='myFile'><input type='submit' name='submit' value= 'Upload'></form> </p>");
+    res.end();
+  });
+
+app.listen(port);
